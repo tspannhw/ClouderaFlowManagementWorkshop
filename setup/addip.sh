c9bd6bbadcbfe27d80bbd9d77d86f24cd9c6fa3d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-04-15 20:40:08 -0400

    Delete addip.sh

--- setup/addip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-
-MYIP=$1
-
-aws ec2 authorize-security-group-ingress --group-id sg-03fc2747e79f25db0 --protocol all --cidr $MYIP/32
-aws ec2 authorize-security-group-ingress --group-id sg-08c61a2a9fee5de5c --protocol all --cidr $MYIP/32
